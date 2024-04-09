610c85244883e5e6f6904b2bcaa117da1a707b24
DariaaaP <barinova_f@list.ru>
2024-04-09 19:11:02 +0400

    added plug img

--- src/components/movieCardView/MovieCardView.jsx
+++ src/components/movieCardView/MovieCardView.jsx
@@ -1,10 +1,15 @@
 import { Card } from "antd";
+import img from "../../resources/img/plug.png";
 const { Meta } = Card;
 
 const MovieCardView = ({ props }) => {
     const { name, poster, year } = props;
+
     return (
-        <Card hoverable cover={<img alt={name} src={poster} />}>
+        <Card
+            hoverable
+            cover={<img alt={name} src={poster === null ? img : poster} />}
+        >
             <Meta title={name} description={year} />
         </Card>
     );
